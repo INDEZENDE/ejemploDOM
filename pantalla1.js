00041cefb09b045bfe6a171e0687810492dc8fb2
Indelisa Rios <Indelisa.Rios@gft.com>
2018-11-12 18:17:49 -0600

    Ejercicio de la rana

--- pantalla1.js
+++ pantalla1.js
@@ -14,21 +14,61 @@ function recibir() {
     for (i = 0; i < valor; i++) {
         // Crea un botón por cada iterción
         var btn = document.createElement("button");
-        btn.innerHTML="Botón"
+        btn.innerHTML = "Botón"
         // Agrego el atributo name por medio de SET con cada iteración
         btn.setAttribute("name", i);
-        btn.addEventListener('click', function(e){
-            let evento =e.target.name;
-            evento=parseInt(evento)+1;
-            alert('Hola soy el botón'+evento );})
+        btn.addEventListener('click', function (e) {
+            let evento = e.target.name;
+            evento = parseInt(evento) + 1;
+            alert('Hola soy el botón' + evento);
+        })
         cont.appendChild(btn)
 
-        if(i%2==0){
+        if (i % 2 == 0) {
             btn.setAttribute("class", "color-par")
-        }
-        else{
+        } else {
             btn.setAttribute("class", "color-non")
         }
     }
+}
+
+function crear() {
+    var grid = document.getElementById("grid").value;
+    gridNumber = parseInt(grid);
+    grid = gridNumber + 1;
+    document.querySelector("#container2").innerHTML = (" ")
+    // Crea un cuadro por cada número de entrada
+    for (i = 1; i < grid; i++) {
+        let gridButton = document.createElement("button");
+        gridButton.innerHTML = (i);
+        gridButton.setAttribute("id", i);
+        document.querySelector("#container2").appendChild(gridButton)
+    }
+
+
+    let formulario = `<form id="formulario" method="Post">
+    <input type="text" id="jump" placeholder="¿Cuántos brincos dará la rana?" />
+    <input class="button" type="button" name="enviar" value="Brincar" onclick="saltar();" /><br>
+    <div id="inputs"></div>
+</form>`;
+    document.querySelector('#container3').innerHTML = (formulario);
+}
+
+function saltar() {
+    jump=document.getElementById("jump").value;
+    // console.log(gridNumber);
+    // console.log(jump);
+
+    if (jump <= gridNumber) {
+        console.log("La posición es", +jump)
+        document.querySelector("#container4").innerHTML = ("La posición es;", +jump)
+    } else {
+        let total=(jump%gridNumber)
+        if(total==0)document.querySelector("#container4").innerHTML = ("La posición es;", ++gridNumber)
+        else document.querySelector("#container4").innerHTML = ("La posición es;", +total)
+    }
+
+
+
 
 }
